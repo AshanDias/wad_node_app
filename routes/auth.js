b031e0c0f97ba99fa9c6865d22da010f8ca24191
ashan <ashandias.info@gmail.com>
2021-05-10 23:15:27 +0530

    Added swagger delete api

--- routes/auth.js
+++ routes/auth.js
@@ -44,6 +44,7 @@ const Schema = mongoose.Schema;
  * @swagger
  * /api/admin/products:
  *   get:
+ *     summary: Get all products
  *     description: Get all products
  *     responses:
  *       200:
@@ -93,7 +94,54 @@ router.get('/admin/products/:id', itemController.getById);
  *         description: The product was not found
  */
 router.post('/admin/products', itemController.post);
+
+
+/**
+ * @swagger
+ * /api/admin/products/{id}:
+ *   put:
+ *     summary: Update the product by id
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The product id
+ *       - in: body
+ *         name: Product 
+ *         schema:
+ *           type: object
+ *         required: true
+ *         description:  product object
+ *     responses:
+ *       200:
+ *         description: The product description by id
+ *       
+ *       404:
+ *         description: The product was not found
+ */
 router.put('/admin/products/:id', itemController.put);
+
+/**
+ * @swagger
+ * /api/admin/products/{id}:
+ *   delete:
+ *     summary: Delete the product by id
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: Delete the product by id
+ *     responses:
+ *       200:
+ *         description: The product delete by id
+ *       
+ *       404:
+ *         description: The product was not found
+ */
 router.delete('/admin/products/:id', itemController.delete);
 
 module.exports = router;
